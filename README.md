b965432d4487608050072b5076841314b91d6205
Mikkel <mikkel@mediagram.dk>
2018-06-16 13:38:38 +0200

    v.0.0.2

--- README.md
+++ README.md
@@ -3,5 +3,9 @@ Vanilla JavaScript slider.
 
 Version history:
 
+# 0.0.2
+- Fade effect added
+- Revers order option added
+
 # 0.0.1
 - Raw slider with no scene change effect
--- slider.js
+++ slider.js
@@ -1,4 +1,4 @@
-// sliderJS v0.0.1
+// sliderJS v0.0.2
 // By Mikkel Lund Clausen / https://www.linkedin.com/in/mikkel-lund-clausen-a7403452/
 // Repo: https://github.com/lundclausenDK/sliderJS
 
@@ -6,18 +6,19 @@
 
 // Image filenames list in order as you like (without extension .jpg, .png etc)
 let imageList = [
-    "http://via.placeholder.com/350x150?text=foo",
-    "http://via.placeholder.com/350x150?text=bar",
-    "http://via.placeholder.com/350x150?text=yolo",
-    "http://via.placeholder.com/350x150?text=piri"
+    "http://wallpoper.com/images/00/44/98/44/landscape-in-argentina_00449844",
+    "http://fibrechannel-europe.com/wp-content/uploads/2018/04/landscape-wallpaper-hd-0-pleasurable-full-hd-1080p-wallpapers-desktop-backgrounds",
+    "http://s1.1zoom.me/b6152/319/Scenery_Sunrises_and_sunsets_Sky_Roads_Stones_Rays_513485_1920x1080",
+    "http://allpicts.in/download/86/Full_HD_Nature_Wallpapers_1080p_Desktop_Green_Landscape_with_Flower"
 ];
 
 let sliderSpecs = {
     containerName: "sliderJS", // ID name for container html div
     imageFileExt: "jpg", // Image files extension
     widescreenPics: true, // Auto stretch images to 100% width, height follows relatively
-    delay: 2000, // Slide scene delay before changing to next image/slide
-    effect: "flip" // Slide change effects: flip / fade / slide
+    delay: 4000, // Slide scene delay before changing to next image/slide
+    effect: "fade", // Slide change effects: flip / fade / slide
+    reversedOrder: false // Reverse slide order
 };
 
 //// SCRIPT SETTINGS - EDIT BELOW AT YOUR OWN RISK ////
@@ -27,6 +28,10 @@ function init() {
 
     if (sliderSpecs.widescreenPics == true) {
         container.className += "sliderJs-widescreen "
+    }
+
+    if (sliderSpecs.reversedOrder == true) {
+        imageList.reverse();
     }
 }
 
@@ -53,6 +58,8 @@ function sceneChanger() {
         switch(sliderSpecs.effect) {
             case "flip":
                 slides[count].className += " sliderJs-flip "
+            case "fade":
+                slides[count].className += " sliderJS-fade "
         }
 
         count--;
